fb563e0f953460a49d75852aa122ee9af24664ab
“Dasmer <dasmersingh@gmail.com>
2025-10-21 22:44:42 -0400

    Initial Commit

---
+++ MyApps/MyAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppsApp.swift
+//  MyApps
+//
+//  Created by Dasmer Singh on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct MyAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
